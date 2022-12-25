88103ac8aedb5975c137add33fb54cb85e948a22
Nexus <egor2001grashin@gmail.com>
2022-12-26 00:30:19 +0300

    finally made login and registration

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,21 +1,72 @@
-import { View, Text, TextInput, Button, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import {
+	View,
+	Text,
+	TextInput,
+	Button,
+	Image,
+	StyleSheet,
+	TouchableOpacity,
+	Alert,
+} from 'react-native';
+
+import api from '../../apiClient';
+import Loader from '../../components/Loader/Loader';
+import AuthContext from '../../contexts/AuthContext';
+
 import BootstrapStyleSheet from 'react-native-bootstrap-styles';
+import styled from 'styled-components';
 
-import styled from "styled-components";
+export default function Login({ navigation }) {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
 
-export default function Login( { onPress } ) {
-   return (
-		<View>
-         <Title>Вход в аккаунт</Title>
-			<Input placeholder='E-Mail' />
-			<Input placeholder='Пароль' />
-         <TouchableOpacity onPress={onPress} style={s.buttonContainer} activeOpacity={0.7}>
-            <Text style={s.buttonText}>Войти</Text>
-         </TouchableOpacity>
-		</View>
-   )
+	const handleLogin = async (e) => {
+		setIsLoading(true);
+		console.log(email);
+		console.log(password);
+		try {
+			const loginResponse = await api.post('auth/login', {
+				email,
+				password,
+			});
+			console.log(loginResponse);
+			Alert.alert('Success', 'You are in!');
+         navigation.navigate('Home');
+		} catch (error) {
+			console.error(error.response.data);
+			Alert.alert('Error', "Couldn't log in");
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	return (
+		<AuthContext.Consumer>
+         {({signIn}) => <View>
+            <Title>Вход в аккаунт</Title>
+            <Input
+               placeholder='E-Mail'
+               onChangeText={setEmail}
+            />
+            <Input
+               placeholder='Пароль'
+               onChangeText={setPassword}
+            />
+            <TouchableOpacity
+               onPress={() => signIn({ email, password })}
+               style={s.buttonContainer}
+               activeOpacity={0.7}
+            >
+               <Text style={s.buttonText}>Войти</Text>
+            </TouchableOpacity>
+            {/*isLoading && <Loader />*/}
+		   </View>
+      }
+      </AuthContext.Consumer>
+	);
 }
-
 
 const Input = styled.TextInput`
    margin-bottom: 20px;
@@ -23,7 +74,7 @@ const Input = styled.TextInput`
    font-size: 20px;
    border-bottom-width: 1px
    border-bottom-color: #cfcfc4
-`
+`;
 
 const Title = styled.Text`
    font-size: 20px;
@@ -33,20 +84,19 @@ const Title = styled.Text`
 `;
 
 const s = StyleSheet.create({
-   
-   buttonContainer: {
-      elevation: 8,
-      backgroundColor: "#4285f4",
-      borderRadius: 15,
-      paddingVertical: 20,
-      paddingHorizontal: 20,
-   },
+	buttonContainer: {
+		elevation: 8,
+		backgroundColor: '#4285f4',
+		borderRadius: 15,
+		paddingVertical: 20,
+		paddingHorizontal: 20,
+	},
 
-   buttonText: {
-      fontSize: 18,
-      color: "#fff",
-      fontWeight: "bold",
-      alignSelf: "center",
-      textTransform: "uppercase",
-   }
+	buttonText: {
+		fontSize: 18,
+		color: '#fff',
+		fontWeight: 'bold',
+		alignSelf: 'center',
+		textTransform: 'uppercase',
+	},
 });
--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,32 +1,78 @@
-import { View, Text, TextInput, Button, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import styled from 'styled-components';
+import { useState } from 'react';
+import { View, Text, TextInput, Button, Image, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+
+import api from '../../apiClient';
+import Loader from '../../components/Loader/Loader';
+
 import BootstrapStyleSheet from 'react-native-bootstrap-styles';
+import styled from 'styled-components';
 
-
-const bootstrapStyleSheet = new BootstrapStyleSheet();
-const { styles } = bootstrapStyleSheet; 
+/* const bootstrapStyleSheet = new BootstrapStyleSheet();
+const { styles } = bootstrapStyleSheet;  */
 
 const Registration = ( { onPress } ) => {
-   console.log(styles)
+   const [fullName, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [avatarUrl, setAvatarUrl] = useState('https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png');
+	const [isLoading, setIsLoading] = useState(false);
+
+   const handleRegistration = async (e) => {
+		setIsLoading(true);
+		console.log(fullName);
+		console.log(email);
+		console.log(password);
+		console.log(avatarUrl);
+		try {
+			const regResponse = await api.post('auth/register', {
+            fullName,
+				email,
+				password,
+            avatarUrl
+			});
+			console.log(regResponse);
+			Alert.alert('Success', 'You are registered!');
+		} catch (error) {
+			console.error(error.response.data);
+			Alert.alert('Error', "Couldn't create an account");
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
 	return (
 		<View>
-         <Title>Создание аккаунта</Title>
-			<View>
-            <Avatar source={{ uri: 'https://reactnative.dev/img/tiny_logo.png'}} />
+			<View style={{ flexDirection: "row"}}>
+            <View style={{ flexDirection: "column" }}>
+               <Avatar source={{ uri: 'https://ionicframework.com/docs/icons/logo-react-icon.png'}} />
+               <InputURL placeholder='Ссылка на картинку' onChangeText={(e) => setAvatarUrl(e)} />
+            </View>
+            <Title>Создание аккаунта</Title>
 			</View>
-			<Input placeholder='Имя' />
-			<Input placeholder='E-Mail' />
-			<Input placeholder='Пароль' />
-         <TouchableOpacity onPress={onPress} style={s.buttonContainer} activeOpacity={0.7}>
+			<Input placeholder='Имя' onChangeText={(e) => setName(e)} />
+			<Input placeholder='E-Mail' onChangeText={(e) => setEmail(e)} />
+			<Input placeholder='Пароль' onChangeText={(e) => setPassword(e)} />
+         <TouchableOpacity onPress={handleRegistration} style={s.buttonContainer} activeOpacity={0.7}>
             <Text style={s.buttonText}>Зарегистрироваться</Text>
          </TouchableOpacity>
+         {isLoading && <Loader />}
 		</View>
 	);
 };
 
+const InputURL = styled.TextInput`
+   margin-top: -30px;
+   width: 125px
+   margin-bottom: 20px;
+   height: 30px;
+   font-size: 13px;
+   border-bottom-width: 1px
+   border-bottom-color: #cfcfc4
+`
+
 const Input = styled.TextInput`
    margin-bottom: 20px;
-   height: 50px;
+   height: 35px;
    font-size: 20px;
    border-bottom-width: 1px
    border-bottom-color: #cfcfc4
@@ -44,7 +90,6 @@ const Avatar = styled.Image`
 const Title = styled.Text`
    font-size: 20px;
    font-weight: bold
-   margin: auto;
    margin-top: 10px
 `;
 
