375e62ae5cab200287c0d5cf4e4ea73c5c3a83df
Nexus <egor2001grashin@gmail.com>
2022-12-28 01:54:12 +0300

    made CustomImage component

---
+++ src/components/CustomImage/CustomImage.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const CustomImage = styled.Image`
+   display: flex;
+   justify-content: center;
+   margin-bottom: 30px;
+   width: 100px;
+   height: 100px;
+   border-radius: 30px;
+   margin-right: 12px;
+`;
+
+export default CustomImage;
