656be4c66fd82cc29e50e0feb5b904bff3330e65
Nexus <egor2001grashin@gmail.com>
2022-12-29 02:47:30 +0300

    refactored Post component

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,10 +1,14 @@
-import { Linking, StyleSheet, Text, View, Image } from 'react-native';
+import { Linking, StyleSheet, Text, View, Image, TouchableWithoutFeedback  } from 'react-native';
 import Loader from '../Loader/Loader';
 import UserInfo from '../UserInfo/UserInfo';
+import CustomImage from '../CustomImage/CustomImage';
 import api from '../../apiClient';
 import { useNavigation } from '@react-navigation/native';
 import AuthContext from '../../contexts/AuthContext';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import Eye from '../../icons/Eye/Eye';
+
+import styled from 'styled-components';
 
 const Post = ({
 	_id,
@@ -15,13 +19,14 @@ const Post = ({
 	viewsCount,
 	commentsCount,
 	tags,
-   text,
+	text,
 	children,
 	isFullPost,
 	isEditable,
 	isLoading,
 }) => {
 	const navigation = useNavigation();
+
 	if (isLoading) {
 		return <Loader />;
 	}
@@ -29,126 +34,93 @@ const Post = ({
 	const onClickRemove = async () => {
 		await api.delete(`/posts/${_id}`);
 		navigation.navigate('Home');
-	}; 
+	};
+
+	const gotoFullPost = () => {
+		navigation.navigate('FullPost', {
+			id: _id,
+		});
+	};
+   
 	return (
-		<View>
-			{isEditable && isFullPost && (
-				<View>
-					<TouchableOpacity
-						onPress={onClickRemove}
-						style={s.buttonContainer}
-						activeOpacity={0.7}
-					>
-						<Text style={s.buttonText}>Удалить пост</Text>
-					</TouchableOpacity>
-				</View>
-			)}
-
-			{imageUrl && (
-				<Image
-					source={{
-						uri: imageUrl,
-					}}
-				/>
-			)}
-
-			<View /* className={styles.wrapper} */>
-				<UserInfo {...creator} additionalText={createdAt} />
-				<View /* className={styles.indention} */>
-					<Text>{title}</Text>
-					<Text /* className={styles.tags} */>
-						{tags.map((name) => `#${name}`).join(' ')}
-					</Text>
-               <Text>{text}</Text>
+		<TouchableWithoutFeedback onPress={gotoFullPost}>
+		<PostWrapper style={styles.shadow}>
+            {imageUrl && (
+               <CustomImage source={{uri: imageUrl}}/>
+            )}
+				<PostDetails>
+					<Title>{title}</Title>
+               {/* <UserInfo {...creator} additionalText={new Date(createdAt)?.toLocaleDateString("ru")} /> */}
+					
+					<Text style={{ fontSize: 16}}>{isFullPost ? text : text.slice(0, 100)}</Text>
 					{children && (
-						<View /* className={styles.content} */>{children}</View>
+						<View>{children}</View>
 					)}
-					<View /* className={styles.postDetails} */>
-						<Text /* className={styles.tags} */>
-							Просмотры: {viewsCount}
-						</Text>
-						<Text /* className={styles.tags} */>
-							Комментарии: {commentsCount}
-						</Text>
-					</View>
-				</View>
-			</View>
-		</View>
+
+               <View>
+                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                     <Eye />
+				         <Text style={{ color: 'grey' }}> {viewsCount} </Text>
+                  </View>
+               <Tags style={{ color: '#0000EE' }}>{tags.map((name) => `#${name}`).join(' ')}</Tags>
+
+                  {isFullPost && (
+                  <TouchableOpacity onPress={gotoFullPost} activeOpacity={0.7}>
+                     <Text>Подробнее</Text>
+                  </TouchableOpacity>
+                  )}
+                  
+
+                  {isEditable && isFullPost && (
+                  <View>
+                     <TouchableOpacity onPress={onClickRemove} activeOpacity={0.7}>
+                        <Text style={{ color: 'red' }}>Удалить пост</Text>
+                     </TouchableOpacity>
+                  </View>
+		            )}
+				   </View>
+
+				</PostDetails>
+
+		</PostWrapper>
+		</TouchableWithoutFeedback>
 	);
 };
-// const Post = ({
-// 	_id,
-// 	title,
-// 	createdAt,
-// 	imageUrl,
-// 	user: creator,
-// 	viewsCount,
-// 	commentsCount,
-// 	tags,
-// 	children,
-// 	isFullPost,
-// 	isEditable,
-// 	isLoading,
-// }) => {
-// 	// const navigation = useNavigation();
 
-// 	if (isLoading) {
-// 		return <Loader />;
-// 	}
 
-// 	const onClickRemove = async () => {
-// 		await api.delete(`/posts/${_id}`);
-// 		navigation.navigate('Home');
-// 	};
-
-// 	return (
-// 		<View>
-// 			{isEditable && isFullPost && (
-// 				<View>
-// 					<TouchableOpacity
-// 						onPress={onClickRemove}
-// 						style={s.buttonContainer}
-// 						activeOpacity={0.7}
-// 					>
-// 						<Text style={s.buttonText}>Удалить пост</Text>
-// 					</TouchableOpacity>
-// 				</View>
-// 			)}
-
-// 			{imageUrl && (
-// 				<Image
-// 					source={{
-// 						uri: imageUrl,
-// 					}}
-// 				/>
-// 			)}
-
-// 			<View /* className={styles.wrapper} */>
-// 				<UserInfo {...creator} additionalText={createdAt} />
-// 				<View /* className={styles.indention} */>
-// 					<Text>{title}</Text>
-
-// 					<Text /* className={styles.tags} */>
-// 						{tags.map((name) => `#${name}`).join(' ')}
-// 					</Text>
-
-// 					{children && (
-// 						<View /* className={styles.content} */>{children}</View>
-// 					)}
-
-// 					<View /* className={styles.postDetails} */>
-// 						<Text /* className={styles.tags} */>
-// 							Просмотры: {viewsCount}
-// 						</Text>
-
-// 						<Text /* className={styles.tags} */>
-// 							Количество комментариев: {commentsCount}
-// 						</Text>
-// 					</View>
-// 				</View>
-// 			</View>
-// 		</View>
-// 	);
-// };
 
 export default Post;
+
+const PostWrapper = styled.View`
+   flex-direction: row;
+   border-radius: 15px;
+   margin-left: 5px;
+   margin-right: 5px;
+   margin-bottom: 3px;
+   padding-bottom: 15px;
+   padding-top: 20px
+   background-color: white;
+`
+
+const PostDetails = styled.View`
+   justify-content: center;
+   flex: 1;
+`
+
+const Title = styled.Text`
+   font-size: 25px;
+   font-weight: 700;
+   align-self: center;
+   margin-top: -15px
+`
+
+const Tags = styled.Text`
+   color: '#0000EE'
+`
+
+const styles = StyleSheet.create({
+   shadow: {
+      elevation: 15,
+      shadowColor: '#171717',
+   },
+});
