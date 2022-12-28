61e7825878b7f8f15837bef5ad471b0377b5018a
Nexus <egor2001grashin@gmail.com>
2022-12-29 02:42:17 +0300

    BUG FIXED !!!! (какой я мудак госпаде)

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "http://Blog-Portal-mobile-app.nexusov.repl.co/",
+   baseURL: "https://Blog-Portal-mobile-app.nexusov.repl.co/",
 });
 
 export default api;
